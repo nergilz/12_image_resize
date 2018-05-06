0b6b94b30d2027c8ca97e2483d21f7c4912d108d
john <nergilz@yandex.ru>
2018-05-06 23:26:59 +0200

    first version

--- README.md
+++ README.md
@@ -1,6 +1,51 @@
-# Image Resizer
+## Image Resizer
 
-[TODO. There will be project description]
+---
+
+### Discription
+
++ The script takes an input image and puts an image with a new size
++ required argument - is the path to the original image
++ optional parameters: 
+    "width" of the resulting image,
+    "height" of the resulting image,
+    "scale" - how many times to increase (maybe less than 1)
+    "output" - path to the resulting file
++ You need a library [pillow](https://pillow.readthedocs.io/en/latest/handbook/index.html) to statr
++ It is recommended to use [virtualenv](https://docs.python.org/3/library/venv.html) 
+
+
+### How to install pillow
+
+```bash
+pip install -r requirements.txt  # or pip3 
+```
+
+
+### Start script example
+
+```bash
+python3 image_resize.py image/kode_rev.jpeg --output result --scale 0.5
+```
+```bash
+python3 image_resize.py image/kode_rev.jpeg --output result --width 200 --height 100 
+```
+
+
+### Get help
+
+```bash
+python3 image_resize.py --help
+```
+
+
+### Requirements
+
+```bash
+Python ver 3.5 (or higher)
+```
+
+---
 
 # Project Goals
 
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,81 @@
+import argparse
+from PIL import Image
+import os
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def resize_scale_image(path_to_original, scale):
+
+    image_handler = Image.open(path_to_original)
+    x_size, y_size = image_handler.size
+    x_size = int(x_size * scale)
+    y_size = int(y_size * scale)
+    return image_handler.resize((x_size, y_size))
+
+
+def resize_width_height_image(path_to_original, width=None, height=None):
+
+    image_handler = Image.open(path_to_original)
+    x_size, y_size = image_handler.size
+    if width is not None:
+        x_size = width
+    if height is not None:
+        y_size = height
+    return image_handler.resize((x_size, y_size))
+
+
+def save_image(path_to_original, path_to_result, image_out):
+
+    x_size, y_size = image_out.size
+    file_name,  file_extension = os.path.basename(path_to_original).split('.')
+    out_file_name = '{}__{}x{}.{}'.format(file_name, x_size, y_size, file_extension)
+    image_out.save(os.path.join(path_to_result, out_file_name))
+
+
+def get_arguments():
+
+    parser = argparse.ArgumentParser(description='Path arguments for resize image')
+    parser.add_argument('path', help='Path to the original image')
+    parser.add_argument('--output', required=False, default='/', help='Path to the modified image')
+    parser.add_argument('--width', type=int, required=False, help='Image width')
+    parser.add_argument('--height', type=int, required=False, help='Image height')
+    parser.add_argument('--scale', type=float, required=False, help='Magnification factor')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    arguments = get_arguments()
+
+    try:
+        if arguments.scale and arguments.width or arguments.scale and arguments.height:
+            raise Exception('"width", "height" can not be specified together with "scale"')
+
+        if arguments.width is None and arguments.scale is None and arguments.height is None:
+            raise Exception('no parameters to resize')
+
+        if arguments.scale:
+            image_out = resize_scale_image(
+                arguments.path,
+                arguments.scale
+                )
+            save_image(
+                arguments.path,
+                arguments.output,
+                image_out
+                )
+
+        if arguments.width or arguments.height:
+            image_out = resize_width_height_image(
+                arguments.path,
+                arguments.width,
+                arguments.height
+                )
+            save_image(
+                arguments.path,
+                arguments.output,
+                image_out
+                )
+
+    except IOError as error:
+        print(' ERROR: {}'.format(error))
+    except Exception as error:
+        print(' ERROR: {}'.format(error))
