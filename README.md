8d8355c072c60151582e21a014603530f1573b7f
john <nergilz@yandex.ru>
2018-05-15 11:05:47 +0200

    all functions is upgrade

--- README.md
+++ README.md
@@ -5,12 +5,13 @@
 ### Discription
 
 + The script takes an input image and puts an image with a new size
-+ required argument - is the path to the original image
++ required argument:
+    `path` the path to the original image
 + optional parameters: 
-    "width" of the resulting image,
-    "height" of the resulting image,
-    "scale" - how many times to increase (maybe less than 1)
-    "output" - path to the resulting file
+    `width` of the resulting image,
+    `height` of the resulting image,
+    `scale` - how many times to increase (maybe less than 1)
+    `output` - path to the resulting file
 + You need a library [pillow](https://pillow.readthedocs.io/en/latest/handbook/index.html) to statr
 + It is recommended to use [virtualenv](https://docs.python.org/3/library/venv.html) 
 
@@ -25,10 +26,10 @@ pip install -r requirements.txt  # or pip3
 ### Start script example
 
 ```bash
-python3 image_resize.py image/kode_rev.jpeg --output result --scale 0.5
+python3 image_resize.py image/grafiti.jpg --output result --scale 0.5
 ```
 ```bash
-python3 image_resize.py image/kode_rev.jpeg --output result --width 200 --height 100 
+python3 image_resize.py image/grafiti.jpg --output result --width 200 --height 100 
 ```
 
 
--- image_resize.py
+++ image_resize.py
@@ -3,107 +3,108 @@ from PIL import Image
 import os
 
 
-def get_image_resize_to_scale(image_handler, scale):
-
-    x_size, y_size = image_handler.size
-    x_size = int(x_size * scale)
-    y_size = int(y_size * scale)
-    return image_handler.resize((x_size, y_size))
-
-
-def get_image_resize_to_width_height(image_handler, width=None, height=None):
-
-    x_size, y_size = image_handler.size
-    if width is not None:
-        x_size = width
-    if height is not None:
-        y_size = height
-    return image_handler.resize((x_size, y_size))
-
-
-def save_image_resizing(path_to_original, path_to_result, image_out):
-
-    x_size, y_size = image_out.size
-    file_name_with_path, file_extension = os.path.splitext(path_to_original)
-    file_name = os.path.split(file_name_with_path)[1]
-    out_file_name = '{}__{}x{}.{}'.format(
-        file_name,
-        x_size,
-        y_size,
-        file_extension
-        )
-    image_out.save(os.path.join(path_to_result, out_file_name))
-
-
-def open_origin_image(path_to_original):
-    return Image.open(path_to_original)
-
-
-def checking_arguments(scale, width, height):
-    parser_error = argparse.ArgumentParser(description='errors in parameters')
-
-    if scale and (width or height):
-        raise parser_error.error('ERROR: scale width height - together')
-
-    if not any([scale, width, height]):
-        raise parser_error.error('ERROR: no parameters in arguments')
-
-
 def get_arguments():
 
     parser = argparse.ArgumentParser(
         description='Path arguments for resize image'
         )
-    parser.add_argument('path',
-                        help='Path to the original image')
-    parser.add_argument('--output',
-                        required=False,
-                        default='.',
-                        help='Path to the modified image')
-    parser.add_argument('--width',
-                        type=int,
-                        required=False,
-                        help='Image width')
-    parser.add_argument('--height',
-                        type=int,
-                        required=False,
-                        help='Image height')
-    parser.add_argument('--scale',
-                        type=float,
-                        required=False,
-                        help='Magnification factor')
+    parser.add_argument(
+        'path',
+        help='Path to the original image'
+        )
+    parser.add_argument(
+        '--output',
+        required=False,
+        default='.',
+        help='Path to the modified image'
+        )
+    parser.add_argument(
+        '--width',
+        type=int,
+        required=False,
+        help='Image width'
+        )
+    parser.add_argument(
+        '--height',
+        type=int,
+        required=False,
+        help='Image height'
+        )
+    parser.add_argument(
+        '--scale',
+        type=float,
+        required=False,
+        help='Change of scale image'
+        )
     return parser.parse_args()
+
+
+def checking_arguments(scale, width, height):
+    parser = argparse.ArgumentParser(description='Errors in parameters')
+
+    if scale and (width or height):
+        parser.error('ERROR: scale width height should not be together')
+
+    if not any([scale, width, height]):
+        parser.error('ERROR: no parameters in arguments')
+
+
+def get_resize_by_scale(x_base, y_base, scale):
+
+    x_size = int(x_base * scale)
+    y_size = int(y_base * scale)
+    return x_size, y_size
+
+
+def get_resize_by_width_height(width, height):
+    return width, height
+
+
+def save_image_resizing(path_to_original, path_to_result, image_out):
+
+    if os.path.isdir(path_to_result):
+        x_size, y_size = image_out.size
+        file_fullname = os.path.basename(path_to_original)
+        file_name, file_extension = os.path.splitext(file_fullname)
+        out_file_name = '{}__{}x{}.{}'.format(
+            file_name,
+            x_size,
+            y_size,
+            file_extension
+            )
+        image_out.save(os.path.join(path_to_result, out_file_name))
+    else:
+        print('ERROR: not find directory "{}"'.format(path_to_result))
 
 
 if __name__ == '__main__':
     arguments = get_arguments()
+    checking_arguments(arguments.scale, arguments.width, arguments.height)
 
     try:
-        checking_arguments(arguments.scale, arguments.width, arguments.height)
-        image_handler = open_origin_image(arguments.path)
+        image = Image.open(arguments.path)
+        x_size, y_size = image.size
 
         if arguments.scale:
-            image_out = get_image_resize_to_scale(
-                image_handler,
+            x_size, y_size = get_resize_by_scale(
+                x_size,
+                y_size,
                 arguments.scale
-                )
-            save_image_resizing(
-                arguments.path,
-                arguments.output,
-                image_out
                 )
 
         if arguments.width or arguments.height:
-            image_out = get_image_resize_to_width_height(
-                image_handler,
+            x_size, y_size = get_resize_by_width_height(
                 arguments.width,
                 arguments.height
                 )
-            save_image_resizing(
-                arguments.path,
-                arguments.output,
-                image_out
-                )
+
+        image_out = image.resize((x_size, y_size))
+
+        save_image_resizing(
+            arguments.path,
+            arguments.output,
+            image_out
+            )
 
     except IOError as error:
-        print(' ERROR: {}'.format(error))
+        print(' ERROR_IOError: {}'.format(error))
